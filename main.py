12974010fb70c61126a505be7c7d70dcc6bdd1ae
Nikhil Advani <93079599+NikhilAdvani@users.noreply.github.com>
2024-07-15 12:34:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import streamlit as st
 import time
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 from dotenv import load_dotenv
 load_dotenv() # take environment variables from .env (especially openai api key)
