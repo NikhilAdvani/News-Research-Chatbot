93d9dbcc9c1e0d65de89c4a7835d6007105fba86
Nikhil Advani <93079599+NikhilAdvani@users.noreply.github.com>
2025-01-22 13:11:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 load_dotenv() # take environment variables from .env (especially openai api key)
 
-# openai_api_key = os.getenv("OPENAI_API_KEY")
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 st.title("News Research Chatbot 📈")
 st.sidebar.title("News Article URLs")
